43354cc820b966291aa325b90300d0669815351d
Roc <psmusicalroc@gmail.com>
2023-11-05 08:39:04 -0500

    wheeeeeeeeeeeeeeeeeeee

---
+++ static/scripts/doTraining.js
@@ -0,0 +1,7 @@
+function getNextExercise(firstime) {
+    let numInput = document.getElementById("num-correct").value;
+    let currentExerciseHTML = document.getElementById("exercise-path");
+    let currentRepetitionsHTML = document.getElementById("exercise-reps");
+
+    let formdata = new FormData();
+}
