db0fcbbc2721868ea0480fbb49e14e359198316b
santapacman <santapacman@gmail.com>
2023-11-04 20:41:50 -0400

    added comments to main.py

--- backend/main.py
+++ backend/main.py
@@ -15,40 +15,59 @@ class Exercise:
         b = "{"
         self.lastPath = -1
         self.lastMult = .5
-        #This piece 
+        #This piece defines the root node.
         if(ind==-1):
             tVar = readString.split(b)
             self.children=[]
             readString = readString[1:]
             tVar = readString.split(b)
+            #Define the root's own aspects
             tVar[0] = tVar[0].split(",")
             self.size = tVar[0][0]
             self.timesCalled = int(tVar[0][1])
             self.name = tVar[0][2]
             self.score = float(tVar[0][3])
            
-            
+            #Define the root's children
             if(len(tVar)>1):
+                #i = 1 because this was a loop and I'm too lazy to change it.
                 i = 1
                 tVar[i] = tVar[i].split(";")
+                #Loop through the layer of nodes adjacent to the
+                #root and initialize them.
                 for j in range(len(tVar[i])):
                     tVar[i][j] = tVar[i][j].split(",")
                     
                     
                     if(j==0):
-                        
+                        #In this call, all the children for the
+                        #layer beneath this one (they would be
+                        #found in tVar[2] here) are initialized
+                        #before the rest of the children in tVar[1] are.
+                        #This is a depth-first recursion.
                         self.children.append(Exercise(readString[readString.index("{"):],1))
                     else:
                         self.children.append(Exercise(tVar[i][j],0,self.children[j-1].children))
+            #likely redundant but not worth removing and finding out.
             else: self.children = []
         elif(ind==0):
             self.name = readString[0]
             self.children = []
             self.score = float(readString[1])
+            #The list of children has already been made for us; it is in
+            #The first node of this layer. It is passed in to us from the
+            #caller.
             for i in range(len(lst)):
                 self.children.append(lst[i])
             
         else:
+            #This is the branch of the function that is called when
+            #Initializing the first node on a new layer. It defines its
+            #own first child (in children[0]--the use of self.children)
+            #before the initialization of a is clunky and ought ot be 
+            #replaced with more time. Once it has the full list of its
+            #grand children from children[0], it can use that list to 
+            #initialize the rest of its children.
             readString = readString[1:]
             
             self.children = readString.split("{")
@@ -68,7 +87,10 @@ class Exercise:
                         self.children.append(Exercise(readString[readString.index("{"):],1))
                     else:
                         self.children.append(Exercise(a[i].split(","),0, self.children[i-1].children))
-                
+           
+#With correct input this function should always work.
+
+#Don't mess with it.
 def parse(filename):      
     with open(filename, "r") as file:
         data = file.read().rstrip()
