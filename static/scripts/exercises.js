19a79ccaeafa5069a223832dc757b8c454f68acf
Roc <psmusicalroc@gmail.com>
2023-11-05 08:54:02 -0500

    unfuck you t.txt

--- static/scripts/exercises.js
+++ static/scripts/exercises.js
@@ -215,7 +215,10 @@ function beginTraining() {
     const request = new XMLHttpRequest();
     request.onreadystatechange = async function() {
         if (request.readyState == 4 && request.status == 200) {
-            console.log(request.responseText);
+            document.open();
+            document.write(request.responseText);
+            document.close();
+            
         } else if (request.readyState == 4 && request.status != 200) {
             console.log('ERROR: ' + request.responseText)
         }
